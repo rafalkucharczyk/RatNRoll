09b3c22ac4ee4c5edff585f4444ba30a6f85b4c1
Lukasz <lukasz.qhash@gmail.com>
2015-06-23 21:39:42 +0200

    Add even nicer icons
    Icons for resolutions 100, 114, 120, 144 and 152 were rendered,
    other are generated by make_all_icons.sh script.

--- proj.ios_mac/ios/make_all_icons.sh
+++ proj.ios_mac/ios/make_all_icons.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for res in 29 40 50 57 58 72 76 80 100 114 144 152; do
-  convert -resize ${res}x${res} Icon-120.png Icon-$res.png
+# 100, 114, 120, 144 and 152 were rendered from paths
+for res in 29 40 50 57 58 72 76 80; do
+  convert -resize ${res}x${res} Icon-152.png Icon-$res.png
 done
