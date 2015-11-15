be3d4117c06d606645652106b1232674bdda5107
Rafal Kucharczyk <rafal.kucharczyk@gmail.com>
2015-11-15 21:11:41 +0100

    Initial version of credits in AboutLayer

--- Classes/AboutLayer.cpp
+++ Classes/AboutLayer.cpp
@@ -1,9 +1,12 @@
 #include "AboutLayer.h"
 
+#include "Menu/MenuLabel.h"
+#include "Base/MipmapSprite.h"
+
 USING_NS_CC;
 
 AboutLayer::AboutLayer()
-    : menuHelper({{{0.5, 0.625}, 0.1, "settings"}, {{0.5, 0.325}, 0.1, "back"}},
+    : menuHelper({{{0.9, 0.2}, 0.1, "settings"}, {{0.5, 0.2}, 0.1, "back"}},
                  std::bind(&AboutLayer::menuItemClicked, this, std::placeholders::_1))
 {
 }
@@ -16,6 +19,17 @@ bool AboutLayer::init()
 
     auto menu = menuHelper.getMenu();
     addChild(menu);
+
+    Sprite *sprite = MipmapSprite::create("menu/credits.png");
+    MenuHelper::positionNode(*sprite, {0.5, 0.75}, 0.1);
+    addChild(sprite);
+
+    MenuLabel *label = MenuLabel::create("LUKASZ\nKUCHARCZYK\n\n"
+                                         "MARTA\nKUCHARCZYK\n\n"
+                                         "RAFAL\nKUCHARCZYK",
+                                         {0.5, 0.55}, 0.03);
+
+    addChild(label);
 
     return true;
 }
