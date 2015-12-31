rafalkucharczyk/RatNRoll

commit e3be4c272493bc77a4b4a3c265d2d11bda17da25

Add more google analytics
- Speed and score on failure,
- Whether restart level button is clicked,
- Game completed screen,
- Score thresholds.

Classes/GameFlow.cpp
@@ -292,6 +292,10 @@ void GameFlow::switchToLevelSceneWithScores(int levelNumber,
             levelLayer->playBackgroundMusic();
         });
         levelLayer->setGameFinishedCallback([this, levelLayer](int score) {
+            std::string action = "GameFinished" + std::to_string(currentLevelNumber);
+            SonarCocosHelper::GoogleAnalytics::sendEvent("", action, "score", score);
+            SonarCocosHelper::GoogleAnalytics::sendEvent("", action, "speed",
+                                                         levelLayer->getRatSpeedLevel());
             this->switchToPostLevelScene(score);
             levelLayer->playBackgroundMusic();
         });
@@ -363,8 +367,10 @@ void GameFlow::switchToPostLevelScene(int score)
                                currentChallenge ? currentChallenge->score : 0,
                                currentChallenge ? currentChallenge->playerName : "");
 
-    postLevelLayer->setRestartLevelCallback(
-        std::bind(&GameFlow::switchToLevelScene, this, currentLevelNumber));
+    postLevelLayer->setRestartLevelCallback([this]() {
+        this->switchToLevelScene(currentLevelNumber);
+        SonarCocosHelper::GoogleAnalytics::sendEvent("", "GameRestarted", "", currentLevelNumber);
+    });
     postLevelLayer->setGotoMainMenuCallback(
         std::bind(&GameFlow::switchToLevelSelectionScene, this));
     postLevelLayer->setShareOnFacebookCallback([this, score]() {
@@ -489,6 +495,8 @@ void GameFlow::overlayGameCompletedScene(LevelLayer *levelLayer)
     gameCompletionLayer->setCompletionConifrmedCallback([this, levelLayer]() {
         this->switchToPostLevelScene(LevelCustomization::gameCompletedScore);
     });
+
+    SonarCocosHelper::GoogleAnalytics::sendEvent("", "GameCompleted", "", currentLevelNumber);
 }
 
 void GameFlow::overlayGameScoreThresholdScene(int gameScore, cocos2d::Scene *parentScene)
@@ -504,6 +512,10 @@ void GameFlow::overlayGameScoreThresholdScene(int gameScore, cocos2d::Scene *par
                                                   gameScoreThreshold.second);
 
         parentScene->addChild(layer, 1);
+
+        SonarCocosHelper::GoogleAnalytics::sendEvent("", "ThresholdCompleted" +
+                                                             std::to_string(currentLevelNumber),
+                                                     "", gameScoreThreshold.first);
     }
 }
 

Classes/LevelLayer.cpp
@@ -745,6 +745,13 @@ void LevelLayer::playBackgroundMusic(float crossFadeDuration) const
         levelCustomization->getBackgroundMusicAsset(), crossFadeDuration);
 }
 
+int LevelLayer::getRatSpeedLevel() const
+{
+    float speed = 0.5 + ((ratSpeed - levelCustomization->getRatSpeedMin()) /
+                         levelCustomization->getRatSpeedStep());
+    return static_cast<int>(speed);
+}
+
 void LevelLayer::runCustomActionOnStart()
 {
     std::shared_ptr<LevelLayerProxyImpl> levelLayerProxy(new LevelLayerProxyImpl(*this, nullptr));

Classes/LevelLayer.h
@@ -80,6 +80,8 @@ class LevelLayer : public RUBELayer
 
     void playBackgroundMusic(float crossFadeDuration = 1.0) const;
 
+    int getRatSpeedLevel() const;
+
   private:
     void runCustomActionOnStart();
     void startDroppingItems();

Classes/SonarCocosHelper/IOSCPPHelper.mm
@@ -404,7 +404,10 @@ void IOSCPPHelper::setGADispatchInterval( int dispatchInterval )
 
 void IOSCPPHelper::sendGAEvent( __String category, __String action, __String label, long value )
 {
-    [[IOSHelper instance] sendGAEvent:[NSString stringWithCString:category.getCString( ) encoding:NSUTF8StringEncoding]: [NSString stringWithCString:action.getCString( ) encoding:NSUTF8StringEncoding]: [NSString stringWithCString:label.getCString( ) encoding:NSUTF8StringEncoding]: [NSNumber numberWithLong:(value)]];
+    [[IOSHelper instance] sendGAEvent:string2NSString(category):
+                                      string2NSString(action):
+                                      string2NSString(label):
+                                      [NSNumber numberWithLong:(value)]];
 }
 #endif
 

commit 5e24bf41e3be9e721efac95f7e0b6e8640ae36a2

BUG FIX: Do not crash when restart level button is clicked
Error introduced in e3be4c272493bc77a4b4a3c265d2d11bda17da25

Classes/GameFlow.cpp
@@ -367,9 +367,10 @@ void GameFlow::switchToPostLevelScene(int score)
                                currentChallenge ? currentChallenge->score : 0,
                                currentChallenge ? currentChallenge->playerName : "");
 
-    postLevelLayer->setRestartLevelCallback([this]() {
-        this->switchToLevelScene(currentLevelNumber);
-        SonarCocosHelper::GoogleAnalytics::sendEvent("", "GameRestarted", "", currentLevelNumber);
+    int _currentLevelNumber = this->currentLevelNumber;
+    postLevelLayer->setRestartLevelCallback([this, _currentLevelNumber]() {
+        this->switchToLevelScene(_currentLevelNumber);
+        SonarCocosHelper::GoogleAnalytics::sendEvent("", "GameRestarted", "", _currentLevelNumber);
     });
     postLevelLayer->setGotoMainMenuCallback(
         std::bind(&GameFlow::switchToLevelSelectionScene, this));