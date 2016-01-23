f5bd6fa972d1d5ba901222cde97a30895c33ac8a
Rafal Kucharczyk <rafal.kucharczyk@gmail.com>
2016-01-23 13:23:17 +0100

    Refine guaranteed score screen
    - Simplify texts,
    - Add animation of dancing rat,
    - Add 'Like us' button.
    
    Also:
    - Make BasicWhiteLayer even more opaque.

--- Classes/BasicWhiteLayer.cpp
+++ Classes/BasicWhiteLayer.cpp
@@ -2,4 +2,4 @@
 
 USING_NS_CC;
 
-bool BasicWhiteLayer::init() { return LayerColor::initWithColor(Color4B(255, 255, 255, 240)); }
+bool BasicWhiteLayer::init() { return LayerColor::initWithColor(Color4B(255, 255, 255, 248)); }
